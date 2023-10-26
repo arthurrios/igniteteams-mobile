4c62caea8f3e2fa417fda03ff58fbd13d6f1123b
arthur <arthur.rios007@gmail.com>
2023-10-26 11:04:13 -0300

    add roboto font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
+/* eslint-disable camelcase */
 import Groups from '@screens/Groups'
 import theme from '@theme/index'
+import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar style="light" />
+      {!fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }

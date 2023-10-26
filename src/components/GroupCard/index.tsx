6f8eae5dc612e232d01f2446dec3562231f66019
arthur <arthur.rios007@gmail.com>
2023-10-26 11:53:47 -0300

    add group card component

---
+++ src/components/GroupCard/index.tsx
@@ -0,0 +1,15 @@
+import { TouchableOpacityProps } from 'react-native'
+import { Container, Icon, Title } from './styles'
+
+type Props = TouchableOpacityProps & {
+  title: string
+}
+
+export function GroupCard({ title, ...props }: Props) {
+  return (
+    <Container {...props}>
+      <Icon />
+      <Title>{title}</Title>
+    </Container>
+  )
+}

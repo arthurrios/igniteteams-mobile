d00e4b17eefad0a31de58d2d72518eb014872a9f
arthur <arthur.rios007@gmail.com>
2023-10-26 11:19:58 -0300

    add header component

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="light" />
+      <StatusBar style="light" translucent backgroundColor="transparent" />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )

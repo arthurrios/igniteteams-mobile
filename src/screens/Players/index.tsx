17bad016fe9ced8eea539579aaa17fd640973597
arthur <arthur.rios007@gmail.com>
2023-10-26 23:36:08 -0300

    add list empty and button to players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,10 +7,12 @@ import { Filter } from '@components/Filter'
 import { FlatList } from 'react-native'
 import { useState } from 'react'
 import { PlayerCard } from '@components/PlayerCard'
+import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState(['Rodrigo', 'Diego', 'Vini', 'Biro'])
+  const [players, setPlayers] = useState([])
 
   return (
     <Container>
@@ -48,7 +50,17 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas neste time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   )
 }

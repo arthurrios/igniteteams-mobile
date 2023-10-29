b2cbb64346da7f4c55bb748c382d713605fb4a23
arthur <arthur.rios007@gmail.com>
2023-10-29 20:55:32 -0300

    add routes type

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react'
 import { FlatList } from 'react-native'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
+import { useNavigation } from '@react-navigation/native'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -27,7 +28,7 @@ export function Groups() {
         showsVerticalScrollIndicator={false}
       />
 
-      <Button type="PRIMARY" title="Criar nova turma" />
+      <Button title="Criar nova turma" />
     </Container>
   )
 }

e0cbf4110787edf597b282d9ae53f579a6d12bd3
arthur <arthur.rios007@gmail.com>
2023-10-30 11:21:52 -0300

    add use focus effect to render groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,11 +2,12 @@ import { Header } from '@components/Header'
 import { Container } from './styles'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { FlatList } from 'react-native'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
+import { groupGetAll } from '@storage/group/groupGetAll'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -16,6 +17,22 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupGetAll()
+      setGroups(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log('useFocusEffect executou')
+      fetchGroups()
+    }, []),
+  )
 
   return (
     <Container>

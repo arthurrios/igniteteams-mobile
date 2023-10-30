eac4eaec0936a3e87e76ef353ca024a31b75d2bd
arthur <arthur.rios007@gmail.com>
2023-10-30 11:43:32 -0300

    add player add by group function

---
+++ src/storage/players/playerAddByGroup.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-useless-catch */
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { AppError } from '@utils/AppError'
+import { PLAYER_COLLECTION } from '@storage/storageConfig'
+import { PlayerStorageDTO } from './PlayerStorageDTO'
+
+export async function playerAddByGroup(
+  newPlayer: PlayerStorageDTO,
+  group: string,
+) {
+  try {
+    await AsyncStorage.setItem(
+      `${PLAYER_COLLECTION}-${group}`,
+      JSON.stringify(newPlayer),
+    )
+  } catch (error) {
+    throw error
+  }
+}

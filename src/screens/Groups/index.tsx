6e72d553d06d990b9008f9c7d5f5440b9fb721ad
arthur <arthur.rios007@gmail.com>
2023-10-29 21:03:53 -0300

    add use navigation to first screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -11,6 +11,12 @@ import { useNavigation } from '@react-navigation/native'
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
 
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
+
   return (
     <Container>
       <Header />
@@ -28,7 +34,7 @@ export function Groups() {
         showsVerticalScrollIndicator={false}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   )
 }
